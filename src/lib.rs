c784a778b68a98bf3cc67b415c79283b46d29efc
Guillaume DIDIER <guillaume.didier.2014@polytechnique.org>
2024-12-19 09:52:54 +0100

    Initial commit of a rough wrapper for libnuma

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = (2 + 2);
+        assert_eq!(result, 4);
+    }
+}
